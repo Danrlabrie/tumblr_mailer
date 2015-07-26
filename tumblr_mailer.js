91015c5b83f624876dd9e2deabd7a5ad70c27c5f
Danrlabrie <danrlabrie@gmail.com>
2015-07-26 16:47:30 -0400

    initial commit also complete

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,6 +1,20 @@
 var fs = require('fs');
+var ejs = require('ejs');
+var tumblr = require('tumblr.js');
 
-var readFile = fs.readFileSync("friend_list.csv","utf8");
+var myCsvFile = fs.readFileSync("friend_list.csv","utf8");
+var myemailTemplate = fs.readFileSync('email_template.ejs', 'utf8');
+
+var client = tumblr.createClient({
+  consumer_key: 'key here',
+  consumer_secret: 'key here',
+  token: 'key here',
+  token_secret: 'key here'
+});
+
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('key here');
+
 
 // Plan: use regex to get an array of line elements
 // ignore the first element, which will be the header - until later when needed for building keys/value pairs
@@ -9,8 +23,7 @@ var readFile = fs.readFileSync("friend_list.csv","utf8");
 // pairs of the header elements and their associated values. refactor for efficiency.
 
 
-var csvParse = function(file) {
-	var csvfile = fs.readFileSync(file,"utf8");
+var csvParse = function(csvfile) {
 	var linesofdata = csvfile.split('\n')
 	var elementsarray = []
 	var outputarray = []
@@ -23,10 +36,73 @@ var csvParse = function(file) {
 		
 		objecto[elementsarray[0][k]] = elementsarray[j][k]
 		}
-	outputarray.push(objecto)	
+	outputarray.push(objecto)
+	 
 	}
-		return outputarray
-}
+	return outputarray	
+}	
 
 
-console.log(csvParse("friend_list.csv"));
+ function sendEmail(to_name, to_email, from_name, from_email, subject, message_html){
+    var message = {
+        "html": message_html,
+        "subject": subject,
+        "from_email": from_email,
+        "from_name": from_name,
+        "to": [{
+                "email": to_email,
+                "name": to_name
+            }],
+        "important": false,
+        "track_opens": true,    
+        "auto_html": false,
+        "preserve_recipients": true,
+        "merge": false,
+        "tags": [
+            "Fullstack_Tumblrmailer_Workshop"
+        ]    
+    };
+    var async = false;
+    var ip_pool = "Main Pool";
+    mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+        // console.log(message);
+        // console.log(result);   
+    }, function(e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
+ }
+
+
+client.posts('jsidneyrocket.tumblr.com', function(err, blog){
+	var latestPosts = [];
+	var thisDate =  new Date() ;
+	blog.posts.forEach(function(post){
+	  	var thisOtherDate = new Date(post.date)
+	  	if (thisDate - thisOtherDate < 604800000) {
+	  		latestPosts.push(post)
+	  	}
+	  
+	  });
+
+
+var myData = (csvParse(myCsvFile));
+
+	 
+myData.forEach(function(row){
+	firstName = row['firstName'];
+	numMonthsSinceContact = row['numMonthsSinceContact'];
+	template = myemailTemplate;
+			
+			var customizedTemplate = ejs.render(template, {firstName: firstName,
+									   numMonthsSinceContact: numMonthsSinceContact,
+									   latestPosts: latestPosts									
+			 });
+
+			sendEmail(firstName, row["emailAddress"], "Daniel Labrie", "danrlabrie@gmail.com", "Hi!", customizedTemplate);			
+			
+		});
+});	
+
+
